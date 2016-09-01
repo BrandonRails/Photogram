e969dd20cf69704879f2ec92e36eee2655cbd7c2
Brandon Stevens <brandon.stevens001@gmail.com>
2016-09-01 13:08:42 -0700

    added paperclip gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,14 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def create 
+  	@user = User.create(user_params)
+  end
+
+  private
+
+   def user_params
+   	params.require(:user).permit(:avatar)
+   end
 end
